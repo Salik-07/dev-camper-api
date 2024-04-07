d8c3c20c122fd01de4c92101efd718e1a1224b81
Salik-07 <salikhussain41@gmail.com>
2024-04-07 16:46:33 +0500

    Add bootcamp routes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const bootcampRouter = require("./routers/bootcamp");
 
 dotenv.config({ path: "./config/dev.env" });
 
 const app = express();
+app.use("/api/v1/bootcamps", bootcampRouter);
 
 app.use(express.json());
 
