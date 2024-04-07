1d76e0b6bdd46680bbddfacfc0b64b9c490df45c
Salik-07 <salikhussain41@gmail.com>
2024-04-07 18:11:38 +0500

    Create mongodb connection

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+require("colors");
 
+const connectDB = require("./db/mongoose");
 const bootcampRouter = require("./routers/bootcamp");
 
+// Load env vars
 dotenv.config({ path: "./config/dev.env" });
+
+// Connect to database
+connectDB();
 
 const app = express();
 
--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ const port = process.env.PORT;
 const mode = process.env.NODE_ENV;
 
 app.listen(port, () => {
-  console.log(`Server running in ${mode} mode on port ${port}`);
+  console.log(`Server running in ${mode} mode on port ${port}`.yellow.bold);
 });
