d88f39d2b609185d22c4671c4154c7699fe63bae
Salik-07 <salikhussain41@gmail.com>
2024-04-07 17:02:06 +0500

    Add logging

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
+
 const bootcampRouter = require("./routers/bootcamp");
 
 dotenv.config({ path: "./config/dev.env" });
 
 const app = express();
-app.use("/api/v1/bootcamps", bootcampRouter);
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 app.use(express.json());
+app.use("/api/v1/bootcamps", bootcampRouter);
 
 module.exports = app;
