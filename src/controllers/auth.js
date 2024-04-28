8f179a88951a4d2aa15ead9e0118eacbc5c78d7f
Salik-07 <salikhussain41@gmail.com>
2024-04-28 18:45:52 +0500

    Add login user functionality

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -17,6 +17,35 @@ const registerUser = asyncHandler(async (req, res) => {
   res.status(200).json({ success: true, token });
 });
 
+const loginUser = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Validate email and password
+  if (!email || !password) {
+    return next(new ErrorResponse("Please provide an email and password", 400));
+  }
+
+  // Check for existing user
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new ErrorResponse("Invalid credentials", 401));
+  }
+
+  // Check if password matches
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return next(new ErrorResponse("Invalid credentials", 401));
+  }
+
+  // Create token
+  const token = await user.generateAuthToken();
+
+  res.status(200).json({ success: true, token });
+});
+
 module.exports = {
   registerUser,
+  loginUser,
 };
