41ea5e4c27236144419e0de2ac66c1cb00739aad
Salik-07 <salikhussain41@gmail.com>
2024-05-08 18:57:26 +0500

    made changes in seeder to upload users

--- seeder.js
+++ seeder.js
@@ -9,6 +9,7 @@ dotenv.config({ path: "./config/dev.env" });
 // Load models
 const Bootcamp = require("./src/models/bootcamp");
 const Course = require("./src/models/course");
+const User = require("./src/models/user");
 
 // Connect to DB
 mongoose.connect(process.env.MONGODB_URL);
@@ -22,11 +23,16 @@ const courses = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/courses.json`, "utf8")
 );
 
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/users.json`, "utf8")
+);
+
 // Import into DB
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
+    await User.create(users);
 
     console.log(`Data imported...`.green.inverse);
     process.exit();
@@ -40,6 +46,7 @@ const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
+    await User.deleteMany();
 
     console.log(`Data destroyed...`.red.inverse);
     process.exit();
