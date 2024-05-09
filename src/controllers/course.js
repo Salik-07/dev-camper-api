8db417c11ed5dd953b3554a11c94f64230375aff
Salik-07 <salikhussain41@gmail.com>
2024-05-09 19:08:47 +0500

    Add ownership in course module

--- src/controllers/course.js
+++ src/controllers/course.js
@@ -52,7 +52,18 @@ const addCourse = asyncHandler(async (req, res, next) => {
     );
   }
 
+  // Make sure user is bootcamp owner
+  if (bootcamp.user.toString() !== req.user._id && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(
+        `User ${req.user._id} is not authorized to add a course to bootcamp ${bootcamp._id}`,
+        401
+      )
+    );
+  }
+
   req.body.bootcamp = req.params.bootcampId;
+  req.body.user = req.user._id;
 
   const course = await Course.create(req.body);
 
@@ -63,16 +74,28 @@ const addCourse = asyncHandler(async (req, res, next) => {
 });
 
 const updateCourse = asyncHandler(async (req, res, next) => {
-  const course = await Course.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true,
-  });
+  let course = await Course.findById(req.params.id);
 
   if (!course) {
     return next(
       new ErrorResponse(`Course not found with id of ${req.params.id}`, 404)
     );
   }
+
+  // Make sure user is course owner
+  if (course.user.toString() !== req.user._id && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(
+        `User ${req.user._id} is not authorized to update course ${course._id}`,
+        401
+      )
+    );
+  }
+
+  course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
 
   res.status(200).json({
     success: true,
@@ -86,6 +109,16 @@ const deleteCourse = asyncHandler(async (req, res, next) => {
   if (!course) {
     return next(
       new ErrorResponse(`Course not found with id of ${req.params.id}`, 404)
+    );
+  }
+
+  // Make sure user is course owner
+  if (course.user.toString() !== req.user._id && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(
+        `User ${req.user._id} is not authorized to delete course ${course._id}`,
+        401
+      )
     );
   }
 
